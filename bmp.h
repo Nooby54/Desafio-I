7f9cac48da1a4f7401f7e7b0952c03f3eb0c284d
Daniel Godoy <danielgodoy54yt@gmail.com>
2025-04-18 15:32:54 -0500

    Añado enmascaramiento al header

--- bmp.h
+++ bmp.h
@@ -13,6 +13,7 @@ public:
     unsigned int* loadSeedMasking(const char* nombreArchivo, int &seed, int &n_pixels);
     unsigned char* rotar_izquierda(unsigned char *ID, unsigned short int n);
     unsigned char* rotar_derecha(unsigned char *ID, unsigned short int n);
+    unsigned char* Enmascaramiento(unsigned char* transformacion, unsigned char* mascara, unsigned int semilla, int totalPixeles);
 };
 
 #endif // BMP_H
