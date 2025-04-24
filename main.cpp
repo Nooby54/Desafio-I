27eca27694a14f65ff151bceec16483994874d44
Daniel Godoy <danielgodoy54yt@gmail.com>
2025-04-24 14:57:02 -0500

    Se elimina la variable transformacion ya que su utilidad inicial perdio sentido, añado comentarios explicando a grandes pasos el main y ademas cambio la ubicacion del informe (el primer commit fue añadiendo ese informe)

--- main.cpp
+++ main.cpp
@@ -5,101 +5,98 @@ using namespace std;
 int main()
 {
     bmp bmp;
-    // Definición de rutas de archivos de entrada
+    // Definición de rutas y variables para cargar ID
     QString I_D = "../../data/I_D.bmp";
     int height_ID = 0;
     int width_ID = 0;
-
     unsigned char *ID = bmp.loadPixels(I_D, width_ID, height_ID);
 
-
+    // Definicion de la ruta y uso de la funcion para contar cuantos archivos .txt hay
     QString ruta = "../../data";
     int n = bmp.contarArchivosMascara(ruta) - 1;
 
-    bool transformacion = false;
-    unsigned char *IT = nullptr;
+    // Definicion de variables adicionales
+    unsigned char *IT = nullptr; // Puntero que va a probar las transformaciones para no afectar ID
     int totalBytes = height_ID * width_ID * 3;
+
     for (int i = n; i >= 0; i--)
     {
+        // Creacion del string con la direccion del archhivo .txt
+        string name = "../../data/M" + to_string(i) + ".txt";
+
+        // Aplicacion del XOR y luego verificamos si es la transformacion adecuada
         delete[] IT;
         IT = bmp.XOR(ID);
-        string name = "../../data/M" + to_string(i) + ".txt";
         if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
         {
-            transformacion = true;
-            cout << "La transformacion " << n-i+1 << " fue un XOR" << endl;
+            cout << "La transformacion " << n - i + 1 << " fue un XOR" << endl;
         }
         else
         {
+            // Si la transformacion no es el XOR, pasa a evaluar las demas transformaciones
             for (int bit = 1; bit <= 8; bit++)
             {
-                if (transformacion == false)
-                {
-                    delete[] IT;
-                    IT = bmp.rotar_derecha(ID, bit, totalBytes);
+                // Aplicacion de la rotacion derecha (inverso de la rotacion izquierda) y validacion
+                delete[] IT;
+                IT = bmp.rotar_derecha(ID, bit, totalBytes);
 
-                    if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
-                    {
-                        transformacion = true;
-                        cout << "La transformacion " << n-i+1 << " fue una rotacion a la izquierda de " << bit << " bits" << endl;
-                        break;
-                    }
+                if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
+                {
+                    cout << "La transformacion " << n - i + 1 << " fue una rotacion a la izquierda de " << bit << " bits" << endl;
+                    break; // Sale del ciclo si la transformacion es correcta
                 }
 
-                if (transformacion == false)
-                {
-                    delete[] IT;
-                    IT = bmp.rotar_izquierda(ID, bit, totalBytes);
+                // Aplicacion de la rotacion izquierda (inverso de la rotacion derecha) y validacion
+                delete[] IT;
+                IT = bmp.rotar_izquierda(ID, bit, totalBytes);
 
-                    if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
-                    {
-                        transformacion = true;
-                        cout << "La transformacion " << n-i+1 << " fue una rotacion a la derecha de " << bit << " bits" << endl;;
-                        break;
-                    }
-                }
-                if (transformacion == false)
+                if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
                 {
-                    delete[] IT;
-                    IT = bmp.desplazamiento_izquierda(ID, bit, totalBytes);
-
-                    if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
-                    {
-                        transformacion = true;
-                        cout << "La transformacion " << n-i+1 << " fue un desplazamiento a la izquierda de " << bit << " bits" << endl;;
-                        break;
-                    }
+                    cout << "La transformacion " << n - i + 1 << " fue una rotacion a la derecha de " << bit << " bits" << endl;
+                    break; // Sale del ciclo si la transformacion es correcta
                 }
 
-                if (transformacion == false)
-                {
-                    delete[] IT;
-                    IT = bmp.desplazamiento_derecha(ID, bit, totalBytes);  // Corregido
+                // Aplicacion del desplazamiento a la derecha (inverso del desplazamiento a la izquierda) y validacion
+                delete[] IT;
+                IT = bmp.desplazamiento_izquierda(ID, bit, totalBytes);
 
-                    if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
-                    {
-                        transformacion = true;
-                        cout << "La transformacion " << n-i+1 << " fue un desplazamiento a la derecha de " << bit << " bits" << endl;
-                        break;
-                    }
+                if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
+                {
+                    cout << "La transformacion " << n - i + 1 << " fue un desplazamiento a la izquierda de " << bit << " bits" << endl;
+                    break; // Sale del ciclo si la transformacion es correcta
+                }
+
+                // Aplicacion del desplazamiento a la izquierda (inverso del desplazamiento a la derecha) y validacion
+                delete[] IT;
+                IT = bmp.desplazamiento_derecha(ID, bit, totalBytes);
+
+                if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
+                {
+                    cout << "La transformacion " << n - i + 1 << " fue un desplazamiento a la derecha de " << bit << " bits" << endl;
+                    break; // Sale del ciclo si la transformacion es correcta
                 }
             }
         }
-        if(transformacion){
-            delete[] ID;
-            ID = bmp.copiar_arreglo(IT, totalBytes);
-        }
-        transformacion = false;
+        // Se elimina ID y le asigno IT, pues en este punto IT ya esta destransformado
+        delete[] ID;
+        ID = bmp.copiar_arreglo(IT, totalBytes);
     }
+    // Se elimina IT
+    delete[] IT;
+
+    // Exportando ID, en este punto ya es IO
     QString I_O = "../../data/I_O.bmp";
-    if(bmp.exportImage(ID,width_ID,height_ID,I_O)){
+    if (bmp.exportImage(ID, width_ID, height_ID, I_O))
+    {
         cout << "Imagen original reconstruida correctamente" << endl;
     }
-    else{
+    else
+    {
         cout << "Ocurrio un error al reconstruir la imagen" << endl;
     }
+
+    //Se elimina ID
     delete[] ID;
-    delete[] IT;
 
     return 0;
 }
