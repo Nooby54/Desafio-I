e6b656277aff2bd1c82606a2e5ad49f6f1f12e8a
Daniel Godoy <danielgodoy54yt@gmail.com>
2025-04-24 16:50:23 -0500

    Cambio el orden de las funciones para que primero pruebe los desplazamientos

--- main.cpp
+++ main.cpp
@@ -36,6 +36,26 @@ int main()
             // Si la transformacion no es el XOR, pasa a evaluar las demas transformaciones
             for (int bit = 1; bit <= 8; bit++)
             {
+                //Aplicacion del desplazamiento a la derecha (inverso del desplazamiento a la izquierda) y validacion
+                delete[] IT;
+                IT = bmp.desplazamiento_izquierda(ID, bit, totalBytes);
+
+                if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
+                {
+                    cout << "La transformacion " << n - i + 1 << " fue un desplazamiento a la izquierda de " << bit << " bits" << endl;
+                    break; // Sale del ciclo si la transformacion es correcta
+                }
+
+                // Aplicacion del desplazamiento a la izquierda (inverso del desplazamiento a la derecha) y validacion
+                delete[] IT;
+                IT = bmp.desplazamiento_derecha(ID, bit, totalBytes);
+
+                if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
+                {
+                    cout << "La transformacion " << n - i + 1 << " fue un desplazamiento a la derecha de " << bit << " bits" << endl;
+                    break; // Sale del ciclo si la transformacion es correcta
+                }
+
                 // Aplicacion de la rotacion derecha (inverso de la rotacion izquierda) y validacion
                 delete[] IT;
                 IT = bmp.rotar_derecha(ID, bit, totalBytes);
@@ -53,26 +73,6 @@ int main()
                 if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
                 {
                     cout << "La transformacion " << n - i + 1 << " fue una rotacion a la derecha de " << bit << " bits" << endl;
-                    break; // Sale del ciclo si la transformacion es correcta
-                }
-
-                // Aplicacion del desplazamiento a la derecha (inverso del desplazamiento a la izquierda) y validacion
-                delete[] IT;
-                IT = bmp.desplazamiento_izquierda(ID, bit, totalBytes);
-
-                if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
-                {
-                    cout << "La transformacion " << n - i + 1 << " fue un desplazamiento a la izquierda de " << bit << " bits" << endl;
-                    break; // Sale del ciclo si la transformacion es correcta
-                }
-
-                // Aplicacion del desplazamiento a la izquierda (inverso del desplazamiento a la derecha) y validacion
-                delete[] IT;
-                IT = bmp.desplazamiento_derecha(ID, bit, totalBytes);
-
-                if (bmp.verificacion_enmascaramiento(IT, name.c_str()))
-                {
-                    cout << "La transformacion " << n - i + 1 << " fue un desplazamiento a la derecha de " << bit << " bits" << endl;
                     break; // Sale del ciclo si la transformacion es correcta
                 }
             }
